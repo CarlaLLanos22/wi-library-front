3a5a3c37bfd5b049d3dedff550199e3bd42e2a0d
Carla Llanos <annacarlallanos@gmail.com>
2023-11-22 11:55:37 -0300

    agregar descuento vista

--- src/componentes/listadoVentas/ListadoVentas.jsx
+++ src/componentes/listadoVentas/ListadoVentas.jsx
@@ -115,6 +115,8 @@ function ListadoVentas() {
                                     </ul>
                                     <label className="block text-gray-700 text-sm font-bold mt-10" htmlFor="Tipo">Total</label>
                                     <b>${venta.total}</b>
+                                    <label className="block text-gray-700 text-sm font-bold mt-10" htmlFor="Tipo">Total con Descuento</label>
+                                    <b>${venta.total-(venta.total-(venta.total*venta.descuento/100))}</b>
                                 </div>
                         </div>
                     </div>
